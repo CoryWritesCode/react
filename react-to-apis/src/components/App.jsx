d915499cfdac46962c46d806507ebb621145c7ad
CoryWritesCode <corycunningham2010@gmail.com>
2018-11-03 11:34:22 -0500

    phase one

--- react-to-apis/src/components/App.jsx
+++ react-to-apis/src/components/App.jsx
@@ -1,7 +1,43 @@
 import React, { Component } from 'react';
+import 'es6-promise';
+import 'isomorphic-fetch';
+import Card from './Card';
 
 class App extends Component {
+  constructor(params) {
+    super(params)
 
+    this.state = {
+      card: []
+    }
+  }
+
+  componentDidMount() {
+    const GHIBLI_API = 'https://ghibliapi.herokuapp.com';
+
+    fetch(`${GHIBLI_API}/films`)
+    .then(res => res.json())
+    .then(obj => {
+      this.setState({
+        card: obj
+      })
+    });
+    console.log(this.state.card);
+  }
+
+  render() {
+    if (!this.state.card.length)
+      return null;
+
+    return (
+
+        this.state.card.map((card, id) => (
+          <Card title={ card.title } director={ card.director } description={ card.description } key={ card.id } />
+        ))
+
+    );
+  }
+  
 }
 
 export default App;
---
+++ react-to-apis/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+function Card(props) {
+  return (
+    <Fragment>
+      <div className="card text-white bg-dark" style={{ innerWidth: '18rem', margin: '20px' }}>
+        <div className="card-body">
+          <h5 className="card-title">{ props.title }</h5>
+          <h6 className="card-subtitle mb-2 text-muted">Directed by { props.director }</h6>
+          <p className="card-text">{ props.description }</p>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Card;
